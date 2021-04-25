9f3afd0158e33ae442d390e7d0d8d85943a3ab94
yugaga525 <71990000+yugaga525@users.noreply.github.com>
2021-04-25 12:59:05 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -88,4 +88,97 @@ function my_keydown(e)
 			console.log("key d");
 		}
 }
+function car1_up()
+{
+    if(car1_y >=0)
+    {
+        car1_y = car1_ -10;
+        console.log("When up arrow is pressed,  x = " + car1_x + " | y = " +car1_y);
 
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+   }
+}
+
+function car1_down()
+{
+    if(car1_y <= 500)
+    {
+        car1_y =car1_y+ 10;
+		console.log("When down arrow is pressed,  x = " + car1_x + " | y = " +car1_y);
+
+
+        uploadBackground();
+		 uploadcar1();
+		 uploadcar2();
+	}
+}
+
+function car1_left()
+{
+	if(car1_x >= 0)
+	{
+		car1_x =car1_x - 10;
+		console.log("When left arrow is pressed,  x = " + car1_x + " | y = " +car1_y);
+		uploadBackground();
+		 uploadcar1();
+		 uploadcar2();
+	}
+}
+
+function car1_right()
+{
+	if(car1_x <= 700)
+	{
+		car1_x =car1_x + 10;
+		console.log("When right arrow is pressed,  x = " + car1_x + " | y = " +car1_y);
+		uploadBackground();
+		uploadcar1();
+		 uploadcar2();
+   }
+}
+function car2_up()
+{
+	if(car2_y >=0)
+	{
+		car2_y = car2_y - 10;
+		console.log("When up arrow is pressed,  x = " + car2_x + " | y = " +car2_y);
+		 uploadBackground();
+		 uploadcar1();
+		 uploadcar2();
+	}
+}
+function car2_down()
+{
+	if(car1_y <=500)
+	{
+		car2_y =car2_y+ 10;
+		console.log("When down arrow is pressed,  x = " + car2_x + " | y = " +car2_y);
+		uploadBackground();
+		 uploadcar1();
+		 uploadcar2();
+	}
+}
+function car2_left()
+{
+	if(car2_x >= 0)
+	{
+		car2_x =car2_x - 10;
+		console.log("When left arrow is pressed,  x = " + car2_x + " | y = " +car2_y);
+		uploadBackground();
+		 uploadcar1();
+		 uploadcar2();
+	}
+}
+function car2_right()
+{
+	if(car2_x <= 700)
+	{
+		car2_x =car2_x + 10;
+		console.log("When right arrow is pressed,  x = " + car2_x + " | y = " +car2_y);
+		uploadBackground();
+		uploadcar1();
+		 uploadcar2();
+   }
+}
